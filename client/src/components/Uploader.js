a95c766865c69c510e98ce57adb8eececfe90d15
Adam Honore <61853978+ahonore42@users.noreply.github.com>
2020-11-10 08:36:26 -0500

    Update Uploader.js

--- client/src/components/Uploader.js
+++ client/src/components/Uploader.js
@@ -1,41 +1,12 @@
 import {__UploadFile} from '../utils/CloudService'
-const CLOUD_NAME = process.env.REACT_APP_CLOUD_NAME
-const UPLOAD_PRESET = process.env.REACT_APP_UPLOAD_PRESET
-
+// env variables here
 
 const Uploader = props => {
-  const widget = window.cloudinary.createUploadWidget(
-    {
-    cloudName: CLOUD_NAME, 
-    uploadPreset: UPLOAD_PRESET, 
-    multiple: false,
-    resourceType: "image", 
-    maxFileSize: 1500000,
-    autoUpload: false
-    },
-    (error, result) => {checkUpload(result)})
-
-  const checkUpload = async (resultEvent) => {
-    if (resultEvent.event === 'success') {
-      try {
-        const url = await resultEvent.info.secure_url
-        if (url) {
-          await __UploadFile(url)
-          await updateRender()
-        } 
-      } 
-      catch(err) {throw err}
-      
-    }
-  }
-
-  const updateRender = async () => {
-    await props.setImages()
-  }
+  // widget and methods here
   
   return (
     <div className="uploader">
-      <button className="btn indigo darken-3" onClick={() => widget.open()}><i className="material-icons left">cloud_upload</i>Upload file</button>
+      
     </div>
   )
 }
